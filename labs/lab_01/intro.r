a44c24c77f8920e81549fb30f25ee66b178e4e3d
benjaminsw <benjamisw@live.com>
2021-01-14 12:05:33 +0000

    added intro.r

---
+++ labs/lab_01/intro.r
@@ -0,0 +1,15 @@
+# Install the fda library & call the fda library
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(fda, psych)
+# Description of Canadian Weather data
+describe(CanadianWeather)
+# Extract the temperature data of the 35 cities
+temp = CanadianWeather$dailyAv
+df_1 = as.data.frame(CanadianWeather$dailyAv)[,1:35]
+# And plot them using matplot 
+matplot(df_1,type="l",xlab="Days")
+# Extract the Vancouver temperature data and plot it
+plot(df_1$Vancouver.Temperature.C,xlab="Days")
+# Extract the Vancouver precipitation data and plot it
+df_2 = as.data.frame(CanadianWeather$dailyAv)[,36:70]
+plot(df_2$Vancouver.Precipitation.mm,xlab="Days")
